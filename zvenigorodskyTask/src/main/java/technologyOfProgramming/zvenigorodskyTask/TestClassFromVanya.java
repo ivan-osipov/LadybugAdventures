046f46cf3d15404c2f9a3250c1a1b68d0d561e38
ivan-osipov <funnyetty.public@yandex.ru>
2014-11-02 21:08:08 +0400

    [+] TestClassFromVanya

---
+++ zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/TestClassFromVanya.java
@@ -0,0 +1,10 @@
+package technologyOfProgramming.zvenigorodskyTask;
+
+public class TestClassFromVanya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
