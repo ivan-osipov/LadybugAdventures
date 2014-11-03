a963e652262b3b14dd5c6e544b185fc64a28be90
ivan-osipov <funnyetty.public@yandex.ru>
2014-11-03 13:37:57 +0400

    [update] pom.xml [*] App

--- zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/App.java
+++ zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/App.java
@@ -16,27 +16,27 @@ import technologyOfProgramming.zvenigorodskyTask.entities.ManagementProgramImpl;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-    	System.out.print("sdfwe");
-    	System.out.print("sdfwes");
-    	File file = new File("C:/Users/osipov/SOFT/eclipse/workspaces/LUNA workspace/zvenigorodskyTask/src/main/resources/managementProgram.xml");
-    	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    	DocumentBuilder dBuilder;
-		try {
-			dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(file);
-//			doc.getDocumentElement().normalize(); 
-			System.out.println(doc.getChildNodes().getLength());
-			new ManagementProgramImpl().setProgram(doc);
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	 
-    	
+    	System.out.println("Hello!");
+    	System.out.println("Bye!");
+//    	File file = new File("C:/Users/osipov/SOFT/eclipse/workspaces/LUNA workspace/zvenigorodskyTask/src/main/resources/managementProgram.xml");
+//    	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+//    	DocumentBuilder dBuilder;
+//		try {
+//			dBuilder = dbFactory.newDocumentBuilder();
+//			Document doc = dBuilder.parse(file);
+////			doc.getDocumentElement().normalize();
+//			System.out.println(doc.getChildNodes().getLength());
+//			new ManagementProgramImpl().setProgram(doc);
+//		} catch (ParserConfigurationException | SAXException | IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+
+
     }
 }
