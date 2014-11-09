35173ca281f9d488fd9becb3834c12295bcce7ca
Evgenya <EvgenyaYaMukhtulova@gmail.com>
2014-11-09 21:50:18 +0400

    [+] HelpFrame [update] App [-] TestClassFromVanya

--- zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/App.java
+++ zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/App.java
@@ -21,7 +21,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println("Hello!");
+    	System.out.println("Hello,World!");
     	System.out.println("Bye!");
 
 //		sample: How get file from resources
