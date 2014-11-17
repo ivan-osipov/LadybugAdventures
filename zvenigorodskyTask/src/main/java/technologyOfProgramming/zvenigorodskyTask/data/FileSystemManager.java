44809072adc38f16aa68a21f204b2b08ed96924f
ivan-osipov <funnyetty.public@yandex.ru>
2014-11-17 23:02:39 +0400

    [update] FSTaskStore reason: add mapStorage

--- zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/data/FileSystemManager.java
+++ zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/data/FileSystemManager.java
@@ -2,6 +2,12 @@ package technologyOfProgramming.zvenigorodskyTask.data;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -12,13 +18,20 @@ import technologyOfProgramming.zvenigorodskyTask.entities.ManagementProgram;
 import technologyOfProgramming.zvenigorodskyTask.exceptions.StorageException;
 
 public class FileSystemManager{
-	private static final String PROGRAM_ADDRESS = "/managementProgram.xml";
+	private static final String PROGRAM_ADDRESS = "/defaultManagementProgram.xml";
+	private static final String MAP_ADDRESS = "/defaultMap.map";
 	private static String programAddress;
 	private static String mapAddress;
 	static{
 		programAddress = PROGRAM_ADDRESS;
+		mapAddress = MAP_ADDRESS;
 	}
-
+	public static void setProgramAddress(String programAddress) {
+		FileSystemManager.programAddress = programAddress;
+	}
+	public static void setMapAddress(String mapAddress) {
+		FileSystemManager.mapAddress = mapAddress;
+	}
 	public static  ManagementProgram getManagementProgramm() throws StorageException {
 		try {
             JAXBContext context = JAXBContext.newInstance(ManagementProgram.class);
@@ -34,7 +47,6 @@ public class FileSystemManager{
 		try {
             JAXBContext context = JAXBContext.newInstance(ManagementProgram.class);
             Marshaller m = context.createMarshaller();
-//            for pretty-print XML in JAXB
             m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
 //             Write to System.out for debugging
@@ -46,9 +58,32 @@ public class FileSystemManager{
             throw new StorageException();
         }
 	}
-	public static void saveManagementProgram(ManagementProgram program, String programAddress) throws StorageException{
-		FileSystemManager.programAddress = programAddress;
-		saveManagementProgram(program);
+
+	public static void saveGameField(Serializable gameField) throws StorageException{
+		File gameFieldFile = new File(mapAddress);
+		if(!gameFieldFile.exists())
+			try {
+				gameFieldFile.createNewFile();
+			} catch (IOException ignore) {
+				throw new StorageException();
+			}
+		try(FileOutputStream fileOutStream = new FileOutputStream(gameFieldFile);
+			ObjectOutputStream serialOutputStream = new ObjectOutputStream(fileOutStream);){
+				serialOutputStream.writeObject(gameField);
+			} catch (IOException e) {
+				throw new StorageException();
+			}
+	}
+	public static Serializable getGameField() throws StorageException{
+		File gameFieldFile = new File(mapAddress);
+		if(!gameFieldFile.exists())
+			throw new StorageException();
+		try(FileInputStream fileInStream = new FileInputStream(mapAddress);
+			ObjectInputStream serialInputStream = new ObjectInputStream(fileInStream);){
+			return (Serializable) serialInputStream.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			throw new StorageException();
+		}
 	}
 //	public static void saveCommand(Command command){
 //		try {
