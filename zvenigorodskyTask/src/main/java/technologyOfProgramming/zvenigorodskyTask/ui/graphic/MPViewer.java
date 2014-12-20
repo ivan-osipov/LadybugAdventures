255139bf34b94ab9bf81267d49e29f3fad84aa0d
Иван <i.osipov.main@gmail.com>
2014-12-20 07:01:42 -0300

    [+] FieldCellRender [+] StartButtonRender [+] resources [+] ResourceProvider [update] MPViewer, GameFieldRenderer

--- zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/ui/graphic/MPViewer.java
+++ zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/ui/graphic/MPViewer.java
@@ -1,11 +1,15 @@
 package technologyOfProgramming.zvenigorodskyTask.ui.graphic;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.lwjgl.LWJGLUtil;
 import org.newdawn.slick.AppGameContainer;
@@ -15,6 +19,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.opengl.ImageDataFactory;
 import org.newdawn.slick.opengl.TextureImpl;
 
 import technologyOfProgramming.zvenigorodskyTask.data.FileSystemManager;
@@ -23,13 +28,19 @@ import technologyOfProgramming.zvenigorodskyTask.entities.ManagementProgram;
 import technologyOfProgramming.zvenigorodskyTask.exceptions.StorageException;
 import technologyOfProgramming.zvenigorodskyTask.ui.MainFrame;
 import technologyOfProgramming.zvenigorodskyTask.ui.graphic.components.GameFieldRenderer;
+import technologyOfProgramming.zvenigorodskyTask.ui.graphic.components.StartButtonRenderer;
+import technologyOfProgramming.zvenigorodskyTask.util.CommonUtils;
+import technologyOfProgramming.zvenigorodskyTask.util.ResourceProvider;
 
 public class MPViewer extends BasicGame {
+	
+	
 	private Image background;
-	private Image buglady;
+	private Image ladybug;
 	private GameFieldRenderer gameField;
 	private GameField startField;
 	private ManagementProgram program;
+	private StartButtonRenderer startButton;
 
 	private float x;
 	private float y;
@@ -46,17 +57,19 @@ public class MPViewer extends BasicGame {
 			throws SlickException {
 		background.draw(0,0,container.getWidth(),container.getHeight());
 		gameField.render(container, g);
-		buglady.draw(x,y);
+		startButton.render(container, g);
+//		buglady.draw(x,y);
 
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		background = new Image(getClass().getResource("/img/pictures/background.jpg").getPath());
-		gameField = new GameFieldRenderer(5,5);
+		background = new Image(ResourceProvider.getResInpStr(ResourceProvider.BACKGROUND_ID),ResourceProvider.BACKGROUND_ID,false);
+		startButton = new StartButtonRenderer(container);
+		gameField = new GameFieldRenderer(20,20);
 		gameField.init(container);
-		buglady = new Image(getClass().getResource("/img/pictures/buglady.png").getPath());
-		buglady.rotate(90);
+		ladybug = new Image(ResourceProvider.getResInpStr(ResourceProvider.LADYBUG_ID),ResourceProvider.LADYBUG_ID,false);
+		ladybug.rotate(90);
 		x=10;
 		y=20;
 	}
@@ -64,6 +77,7 @@ public class MPViewer extends BasicGame {
 	@Override
 	public void update(GameContainer container, int t)
 			throws SlickException {
+		startButton.update(container, t);
 		//x+=0.1*delta;
 
 
@@ -76,29 +90,32 @@ public class MPViewer extends BasicGame {
 	 *
 	 *
 	 */
-//	public static void main(String[] args) {
-//		System.out.println(System.getProperty("java.library.path"));
-//		Path resourcePath = null;
-//		URL resourceUrl = MPViewer.class.
-//				getResource("../lib/native/win32");
+	public static void main(String[] args) {
+		Path resourcePath = null;
+		
+//		URL resourceUrl = AnimationRunner.class.
+//				getResource("/native/win32");
 //		try {
+			System.out.println(CommonUtils.getJarPath());
+			resourcePath = Paths.get(CommonUtils.getJarPath()+"/lib/native/win32/");
 //			resourcePath = Paths.get(resourceUrl.toURI());
-//			System.out.println(resourcePath);
+			System.out.println(resourcePath);
 //		} catch (URISyntaxException e1) {
 //		}
-//		System.setProperty("org.lwjgl.librarypath", resourcePath.toString());
-//		System.setProperty("java.library.path", resourcePath.toString());
-//
-//		try {
-//			AppGameContainer container = new AppGameContainer(new MPViewer(FileSystemManager.getDefaultGameField(),FileSystemManager.getDefaultManagementProgramm()));
-//			container.setDisplayMode(800, 600, false);
-//			
-//			container.setTargetFrameRate(60);
-//			container.start();
-//		} catch (SlickException | StorageException e) {
-//			e.printStackTrace();
-//		}
-//
-//	}
+		System.setProperty("org.lwjgl.librarypath", resourcePath.toString());
+		System.setProperty("java.library.path", resourcePath.toString());
+
+		try {
+			ManagementProgram mp = FileSystemManager.getManagementProgramm("C:/Users/Ivan/Desktop/managementProgram.xml");
+			AppGameContainer container = new AppGameContainer(new MPViewer(FileSystemManager.getGameField(mp.getMapAddress()),mp));
+			container.setDisplayMode(800, 600, false);
+			
+			container.setTargetFrameRate(60);
+			container.start();
+		} catch (SlickException | StorageException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
--- zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/ui/graphic/components/GameFieldRenderer.java
+++ zvenigorodskyTask/src/main/java/technologyOfProgramming/zvenigorodskyTask/ui/graphic/components/GameFieldRenderer.java
@@ -2,6 +2,8 @@ package technologyOfProgramming.zvenigorodskyTask.ui.graphic.components;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.SlickException;
 
 import technologyOfProgramming.zvenigorodskyTask.entities.GameField;
 
@@ -11,6 +13,10 @@ public class GameFieldRenderer extends GameField{
 	private int cellSize;
 	private int fieldWidth;
 	private int fieldHeight;
+	private int renderPosX;
+	private int renderPosY;
+	private GameContainer container;
+	private FieldCellRenderer[][] emptyCellCollection;
 
 	public GameFieldRenderer(GameField field){
 		super(field.getWidth(), field.getHeigh());
@@ -19,10 +25,18 @@ public class GameFieldRenderer extends GameField{
 	public GameFieldRenderer(int width, int height) {
 		super(width, height);
 	}
-	public void render(GameContainer container, Graphics g){
-		g.fillRect(25, 50, fieldWidth, fieldHeight);
+	public void render(GameContainer container, Graphics g) throws SlickException{
+		g.setColor(new Color(152,251,152,0.4f));
+		g.fillRect(renderPosX, renderPosY, fieldWidth, fieldHeight);
+		for(int i = 0; i<getWidth(); i++){
+			for(int j = 0; j<getHeigh(); j++){
+				emptyCellCollection[i][j].render(container, g);
+			}
+		}
+		
 	}
-	public void init(GameContainer container){
+	public void init(GameContainer container) throws SlickException{
+		this.container = container;
 		maxWidth = container.getWidth()-50;
 		maxHeigth = container.getHeight()-150;
 		int cellWidth =  maxWidth / getWidth();
@@ -30,6 +44,29 @@ public class GameFieldRenderer extends GameField{
 		cellSize = cellHeigth>cellWidth? cellWidth : cellHeigth;
 		fieldWidth = cellSize*getWidth();
 		fieldHeight = cellSize*getHeigh();
+		renderPosX = (container.getWidth()-fieldWidth)/2;
+		renderPosY = (container.getHeight()-100-getFieldHeight())/2;
+		emptyCellCollection = new FieldCellRenderer[getWidth()][getHeigh()];
+		for(int i = 0; i<getWidth(); i++){
+			for(int j = 0; j<getHeigh(); j++){
+				emptyCellCollection[i][j] = new FieldCellRenderer(container, cellSize, cellSize);
+				emptyCellCollection[i][j].setLocation(renderPosX+i*cellSize, renderPosY + j*cellSize);
+			}
+		}
 	}
+	
+	public int getFieldWidth() {
+		return fieldWidth;
+	}
+	public void setFieldWidth(int fieldWidth) {
+		this.fieldWidth = fieldWidth;
+	}
+	public int getFieldHeight() {
+		return fieldHeight;
+	}
+	public void setFieldHeight(int fieldHeight) {
+		this.fieldHeight = fieldHeight;
+	}
+	
 
 }
